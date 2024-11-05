30e256403d33ed6022af6b036ed8196600cc032e
rbkh17willis <124805813+rbkh17willis@users.noreply.github.com>
2024-11-04 18:34:42 -0700

    allowed host added

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fierce-wildwood-90358-8311fe88c086.herokuapp.com']
 
 
 # Application definition
