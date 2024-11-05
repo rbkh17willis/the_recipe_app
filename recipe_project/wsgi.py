4fd9375471b9e956efbc5587c74efde40ba22481
rbkh17willis <124805813+rbkh17willis@users.noreply.github.com>
2024-11-04 19:50:20 -0700

    change to recipe_project

--- recipe_project/wsgi.py
+++ recipe_project/wsgi.py
@@ -14,6 +14,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookstore.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recipe_project.settings')
 
 application = get_wsgi_application()
